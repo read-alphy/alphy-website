ec168f74156f75132e4786fc07a6e4ef6bffc5ef
cagri <mcagridurgut@gmail.com>
2023-02-05 14:56:17 +0300

    footer_updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,45 +18,50 @@ function Footer() {
         
                 </div> */
 
-		<footer class="p-4 sm:p-6 ">
+		<footer class="p-4 sm:p-6 flex flex-col justify center items-center">
+			<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 			<div class="container md:flex md:justify-between mx-auto">
 				<div class="mb-6 md:mb-0 ml-100">
 					<a href="https://flowbite.com/" class="flex items-center">
-						{/* <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" /> */}
 						<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Alphy</span>
 					</a>
-					<p className="text-l font-semibold whitespace-nowrap dark:text-white">
+					<p className="text-l font-semibold sm:whitespace-nowrap dark:text-white">
 						<br />
-						Feel free to reach us through <span className="text-yellow-600"> info@alphy.tech</span> or ping
-						us on our Discord for any questions and feedback.
+						Feel free to reach us through{' '}
+						<a href="mailto:info@alphy.tech">
+							<span className="text-yellow-600 hover:text-yellow-800 cursor-pointer">
+								{' '}
+								info@alphy.tech
+							</span>{' '}
+						</a>
+						or ping us on our Discord for any questions and feedback.
 					</p>
 				</div>
-				<div class="grid grid-cols-1 gap-8 sm:gap-6 sm:grid-cols-1">
+				<div className="grid grid-cols-1 gap-8 sm:gap-6 sm:grid-cols-1">
 					<div>
-						<h2 class="mb-6 mt-4 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+						<h2 className="sm:mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white text-center">
 							Reach us
 						</h2>
-						<ul class="text-gray-600 dark:text-gray-400">
-							<li class="mb-4">
-								<a href="mailto:info@alphy.tech" class="hover:underline ">
-									info@alphy.tech
+						<ul className="text-gray-600 dark:text-gray-400 flex justify-center sm:justify-between">
+							<li className="m-2">
+								<a href="mailto:info@alphy.tech" className="m-2">
+									<img src="/mail.svg" alt="mail" className="sm:w-[50px] w-[20px] opacity-75" />
 								</a>
 							</li>
-							<li class="mb-4">
-								<a href="https://discord.gg/" class="hover:underline">
-									Discord
+							<li className="m-2">
+								<a href="https://discord.gg/" className="m-2">
+									<img src="/discord.svg" alt="discord" className="sm:w-[50px] w-[20px] opacity-75" />
 								</a>
 							</li>
-							<li>
-								<a href="https://twitter.com" class="hover:underline">
-									Twitter
+							<li className={'m-2'}>
+								<a href="https://twitter.com" className="m-2">
+									<img src="/twitter.svg" alt="twitter" className="sm:w-[50px] w-[20px] opacity-75" />
 								</a>
 							</li>
 						</ul>
 					</div>
 				</div>
 			</div>
-			<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 		</footer>
 	);
 }
