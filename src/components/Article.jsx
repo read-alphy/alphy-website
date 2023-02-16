9c327a6d2f5eec0765edba36abab492e5a01c085
chinchin <86435165+chinekop@users.noreply.github.com>
2023-02-16 12:42:07 +0300

    trial auth log out

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -142,7 +142,7 @@ function Article({
 											Home
 										</Link>
 									</div>
-									{sessionContext.userId ? (
+									{sessionContext.userId.length !== 0 ? (
 										<div className="w-1/3 ml-5 mb-5">
 											<Link className="text-l font-semibold text-blueLike" onClick={handleSignOut}>
 												Log Out
