c120e9db267539cc7f8001f11ab6de4a4ba109ee
chinchin <86435165+chinekop@users.noreply.github.com>
2023-02-16 15:19:32 +0300

    sessioncontext

--- src/components/Landing_page/Welcome.jsx
+++ src/components/Landing_page/Welcome.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useState } from 'react';
 import Languages from './Languages';
-import { useSessionContext } from 'supertokens-auth-react/recipe/session';
+import Session from 'supertokens-auth-react/recipe/session';
 import toast, { Toaster } from 'react-hot-toast';
 import Loading from '../Loading';
 import ReactLoading from 'react-loading';
@@ -17,13 +17,14 @@ export default function Welcome() {
 	const [language, setLanguage] = useState('en-US');
 
 	const [loading, setLoading] = useState(false);
-	const sessionContext = useSessionContext();
+	const sessionContext = Session.getUserId();
+	console.log("sessionContext")
 	const navigate = useNavigate();
 	const handleSubmit = (event, selectedOption) => {
 		event.preventDefault();
 		toast.dismiss();
 		// Do something with the inputValue here	
-		if (sessionContext.userId.length !== 0) {
+		if (sessionContext.length !== 0) {
 			if (
 				inputValue.includes('https://www.youtube.com') ||
 				inputValue.includes('https://youtu.be') ||
@@ -117,7 +118,7 @@ export default function Welcome() {
 
 					<Toaster />
 					<div className="flex justify-center">
-						{sessionContext.userId.length !== 0 ? (
+						{sessionContext.length !== 0 ? (
 							<button
 								className="w-1/3 border-2 border-bordoLike px-8 bg-lightblueLike text-whiteLike py-2 mt-6 duration-300 rounded-md lg:mt-0 md:w-auto lg:w-auto hover:opacity-75"
 								type="submit"
