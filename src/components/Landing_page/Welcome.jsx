895d705d7719406a9edb7423203d0a2ca2672ce9
chinchin <86435165+chinekop@users.noreply.github.com>
2023-02-26 14:19:14 +0300

    preventdefault

--- src/components/Landing_page/Welcome.jsx
+++ src/components/Landing_page/Welcome.jsx
@@ -24,7 +24,7 @@ export default function Welcome() {
 
 
 	const handleSubmit = (event, selectedOption) => {
-
+		event.preventDefault()
 		toast.dismiss();
 
 
@@ -134,7 +134,7 @@ export default function Welcome() {
 				<form
 					className="items-center justify-center w-full lg:space-x-4 lg:flex"
 					action="#"
-					onSubmit={(e) => { handleSubmit(e, language); return false }}
+					onSubmit={(e) => { handleSubmit(e, language) }}
 				>
 					<div className="flex items-center mt-4 space-x-4 md:justify-center lg:mt-0">
 
@@ -161,7 +161,7 @@ export default function Welcome() {
 							<button
 								className="w-1/3 border-2 border-bordoLike px-8 bg-lightblueLike text-whiteLike py-2 mt-6 duration-300 rounded-md lg:mt-0 md:w-auto lg:w-auto hover:opacity-75"
 								type="submit"
-								onClick={(e) => handleSubmit(e, language)}
+
 							>
 								Submit
 							</button>
@@ -169,7 +169,7 @@ export default function Welcome() {
 							<button
 								className="w-2/3 border-2 border-bordoLike px-8 bg-lightblueLike text-whiteLike py-2 mt-6 duration-300 rounded-md lg:mt-0 md:w-auto lg:w-auto hover:opacity-75"
 								type="submit"
-								onClick={(e) => handleSubmit(e, language)}>
+							>
 
 								Sign In To Submit
 							</button>
