21464dfe38ad5ed203bf6af6d717d20e558a4509
chinchin <86435165+chinekop@users.noreply.github.com>
2023-02-26 12:48:19 +0300

    fix

--- src/components/Article_components/QuestionAnswering.jsx
+++ src/components/Article_components/QuestionAnswering.jsx
@@ -201,7 +201,7 @@ export default function QuestionAnswering(source_id, key_qa, data) {
                         </div>
 
                         <input value={inputValue} onClick={() => handleClick(true)} onChange={(event) => setInputValue(event.target.value)} onKeyDown={handleKeyDown} type="text" id="search" className=" block w-full p-4 pl-10 text-sm text-zinc-500 placeholder:text-zinc-90  border border-zinc-200 placeholder:italic rounded-lg bg-whiteLike focus:outline-none" placeholder="Ask anything to the transcript..." autoComplete="off" required />
-                        {showBaseQA || answerData.length > 0 ? <div class="absolute inset-y-0 right-0 flex items-center pl-3 pointer-events-none">
+                        {showBaseQA || answerData.length > 0 ? <div onClick={handleClear} class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                             <svg width="20" aria-hidden="true" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M6 18L18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round"></path>
                             </svg>
