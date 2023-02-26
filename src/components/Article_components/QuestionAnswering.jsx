5451074cab7ef818da583a1c76e7f02f827999f0
chinchin <86435165+chinekop@users.noreply.github.com>
2023-02-26 09:42:09 +0300

    reset button

--- src/components/Article_components/QuestionAnswering.jsx
+++ src/components/Article_components/QuestionAnswering.jsx
@@ -226,7 +226,7 @@ export default function QuestionAnswering(source_id, key_qa, data) {
                         </div>)
                         :
 
-                        (<button className="mx-auto text-zinc-700"><svg aria-hidden="true" fill="none" width="30px" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        (<button onClick={handleClear} className="mx-auto text-zinc-700"><svg aria-hidden="true" fill="none" width="30px" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" stroke-linecap="round" stroke-linejoin="round"></path>
                         </svg>
                         </button>)
