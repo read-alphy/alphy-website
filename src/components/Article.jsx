c56eb66aeb030d50893507e4e2bdda0781e52fe4
cagri <mcagridurgut@gmail.com>
2023-02-27 17:26:41 +0300

    article page smooth scrolls

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,7 +4,7 @@ import SideFeed from './Article_components/SideFeed';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Content from './Article_components/ContentTabs/Content';
 
-import { signOut } from "supertokens-auth-react/recipe/passwordless";
+import { signOut } from 'supertokens-auth-react/recipe/passwordless';
 
 import axios from 'axios';
 import Loading from './Loading';
@@ -23,11 +23,10 @@ function Article({
 	offset,
 	setOffset,
 }) {
-
 	const location = useLocation();
 	const navigate = useNavigate();
 
-	const windlowLocationArticle = useLocation().pathname.includes("/article")
+	const windlowLocationArticle = useLocation().pathname.includes('/article');
 	/* 	const windowSize = useWindowSize();
 	useEffect(() => {
 			if (windowSize.width > 768) {
@@ -61,14 +60,13 @@ function Article({
 	};
 	useEffect(() => {
 		const source_id = location.pathname.split('/')[2];
+		console.log(`${process.env.REACT_APP_API_URL}/summaries/youtube/${source_id}`, 'url');
 		const url = `${process.env.REACT_APP_API_URL}/summaries/youtube/${source_id}`;
 		fetchData(url);
 	}, [location.pathname, navigate]);
 
 	// use effect every render
-	useEffect(() => {
-
-	}, []);
+	useEffect(() => {}, []);
 
 	const sideFeed = useMemo(
 		() => (
@@ -87,35 +85,29 @@ function Article({
 		[feedData, setFeedData, feedLoading, setFeedLoading, search, setSearch, offset, setOffset, collapsed],
 	);
 
-
 	return (
 		<div className="article max-[920h] ">
 			<div className="flex flex-row article-body ">
 				<div className={`user-feed flex ${collapsed ? '' : ' mr-5'}`}>
-
 					<div className={`w-[400px] overflow-x-hidden hidden lg:block`}>
 						<div className="bg-zinc-50 mr-5">
 							<div className="user-feed bg-zinc-100">{sideFeed}</div>
 						</div>
 					</div>
-
 				</div>
 				{!collapsed ? ( // hamburger menu for mobile devices
-
 					<div className="fixed top-0 z-50 transition origin-top-right transform md:hidden mb-auto pt-[2px]">
 						<div className="rounded-lg rounded-t-none shadow-lg bg-whiteLike">
 							<div className="h-screen px-4 overflow-y-auto">
-								<div className="flex items-center justify-end p-4 ">
-
-								</div>
+								<div className="flex items-center justify-end p-4 "></div>
 								<div className="grid grid-row">
 									<div className="grid grid-cols-2">
 										<p className=" ml-5 text-xl font-bold text-blueLike pb-10">ALPHY</p>
 
 										<button
-											className={`mb-10 w-1/12 justify-self-end mr-5 ${collapsed ? "hidden" : "block"}`}
-
-
+											className={`mb-10 w-1/12 justify-self-end mr-5 ${
+												collapsed ? 'hidden' : 'block'
+											}`}
 											onClick={() => setCollapsed(true)}
 										>
 											{/* cross svg */}
@@ -136,38 +128,54 @@ function Article({
 										</button>
 									</div>
 									<div className="w-1/3 ml-5 mb-5">
-										<a href="/#feedback" type="button" className={`text-blueLike font-semibold `} onClick={() => setCollapsed(true)}>Give us feedback!</a>
+										<a
+											href="/#feedback"
+											type="button"
+											className={`text-blueLike font-semibold `}
+											onClick={() => setCollapsed(true)}
+										>
+											Give us feedback!
+										</a>
 									</div>
 									<div className="w-1/3 ml-5 mb-5">
-										<Link className="text-l font-semibold text-blueLike" to="/" onClick={() => setCollapsed(true)}>
+										<Link
+											className="text-l font-semibold text-blueLike"
+											to="/"
+											onClick={() => setCollapsed(true)}
+										>
 											Home
 										</Link>
 									</div>
 									{sessionContext.doesSessionExist ? (
 										<div className="w-1/3 ml-5 mb-5">
-											<Link className="text-l font-semibold text-blueLike" onClick={handleSignOut}>
+											<Link
+												className="text-l font-semibold text-blueLike"
+												onClick={handleSignOut}
+											>
 												Log Out
 											</Link>
 										</div>
 									) : (
 										<div className="w-1/3 ml-5 mb-5">
-											<Link className="text-l font-semibold text-blueLike" to="/auth " onClick={() => setCollapsed(true)}>
+											<Link
+												className="text-l font-semibold text-blueLike"
+												to="/auth "
+												onClick={() => setCollapsed(true)}
+											>
 												Sign In
 											</Link>
 										</div>
 									)}
 								</div>
-								<div className={`mt-4 ${windlowLocationArticle ? ('block') : ('block')}`}>
+								<div className={`mt-4 ${windlowLocationArticle ? 'block' : 'block'}`}>
 									<div className="">{sideFeed}</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
 				) : (
 					<></>
 				)}
-
 
 				<div className="scrolling px-4 mx-auto h-[90vh]  ">
 					{isLoading || data.length ? <Loading /> : <Content data={data} />}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ import { Link, useLocation } from 'react-router-dom';
 // import Logout from "../supertokens_home/Logout"
 
 import { useSessionContext } from 'supertokens-auth-react/recipe/session';
-import { signOut } from "supertokens-auth-react/recipe/passwordless";
+import { signOut } from 'supertokens-auth-react/recipe/passwordless';
 import { useNavigate } from 'react-router-dom';
-import Logo from "../img/logo.png"
+import Logo from '../img/logo.png';
 
 function Navbar({ collapsed, setCollapsed }) {
 	let sessionContext = useSessionContext();
@@ -18,16 +18,13 @@ function Navbar({ collapsed, setCollapsed }) {
 	const navigate = useNavigate();
 	const location = useLocation();
 
-
 	const handleSignOut = async () => {
 		try {
 			await signOut();
 
-			if (location.pathname === "/") {
+			if (location.pathname === '/') {
 				window.location.reload();
-
-			}
-			else {
+			} else {
 				navigate('/');
 			}
 		} catch (error) {
@@ -35,81 +32,65 @@ function Navbar({ collapsed, setCollapsed }) {
 		}
 	};
 
-
-
-
 	return (
 		<div className="navbar  bg-bordoLike text-slate-100 font-bold max-h-[10vh]">
 			<div className="pl-10 flex flex-row ">
 				<Link to="/">
-
 					{/* <img className="w-10" src={Logo} /> */}
 					<h1 className="text-2xl">ALPHY</h1>
-
 				</Link>
-
 			</div>
-
-
 
 			<div>
 				<div className=" navbar-right-section">
-
 					<div className="signed-in-navbar grid gap-4">
+						<a
+							href="/#feedback"
+							type="button"
+							className={`hidden md:block text-zinc-600 font-semibold bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700  rounded-lg text-sm px-5 py-2.5 text-center mr-4`}
+						>
+							Give us feedback!
+						</a>
 
-						<a href="/#feedback" type="button" className={`hidden md:block text-zinc-600 font-semibold bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700  rounded-lg text-sm px-5 py-2.5 text-center mr-4 mb-2`}>Give us feedback!</a>
+						<a href="/#about" className="hidden font-semibold text-zinc-200 md:block mr-5 pt-2">
+							{' '}
+							About{' '}
+						</a>
 
-						<a href="/#about" className="hidden font-semibold text-zinc-200 md:block mr-5 pt-2"> About </a>
-
-
-						{sessionContext.userId ? (<div className="hidden md:block pt-2">
-
-							<Link className="text-l font-semibold text-zinc-200" onClick={handleSignOut}>
-								Log Out
-							</Link>
-
-						</div>) : (
-
+						{sessionContext.userId ? (
+							<div className="hidden md:block pt-2">
+								<Link className="text-l font-semibold text-zinc-200" onClick={handleSignOut}>
+									Log Out
+								</Link>
+							</div>
+						) : (
 							<div className="hidden md:block font-semibold pt-2 text-zinc-200">
-
 								<Link to="/auth">Sign In</Link>
 							</div>
 						)}
 
-						<div
-							onClick={() => setCollapsed(!collapsed)}
-							className={`block mr-4 cursor-pointer md:hidden`}
-						>
+						<div onClick={() => setCollapsed(!collapsed)} className={`block mr-4 cursor-pointer md:hidden`}>
 							<i className="text-2xl ri-menu-line text-mainText"></i>
-
-
 						</div>
-
-
 					</div>
-
 				</div>
 			</div>
 
 			{!collapsed ? ( // hamburger menu for mobile devices
-
 				<div className="fixed top-0 z-50 transition origin-top-left w-full transform md:hidden mb-auto pt-[2px]">
 					<div className="rounded-lg rounded-t-none shadow-lg bg-whiteLike">
 						<div className="h-screen px-4 overflow-y-auto">
-							<div className="flex items-center justify-end p-4 ">
-
-							</div>
+							<div className="flex items-center justify-end p-4 "></div>
 							<div className="grid grid-row">
 								<div className="grid grid-cols-2">
 									<p className=" ml-5 text-xl font-bold text-blueLike pb-10">ALPHY</p>
 
 									<button
-										className={`mb-10 w-1/12 justify-self-end mr-5 text-blueLike ${collapsed ? "block" : "block"}`}
-
-
+										className={`mb-10 w-1/12 justify-self-end mr-5 text-blueLike ${
+											collapsed ? 'block' : 'block'
+										}`}
 										onClick={() => setCollapsed(true)}
 									>
-
 										<svg
 											className="w-5 h-5"
 											fill="bg-blueLike"
@@ -124,18 +105,31 @@ function Navbar({ collapsed, setCollapsed }) {
 												d="M6 18L18 6M6 6l12 12"
 											></path>
 										</svg>
-
 									</button>
 								</div>
 								<div className="w-1/3 ml-5 mb-5">
-									<a href="/#feedback" type="button" className={`text-blueLike font-semibold `} onClick={() => setCollapsed(true)}>Give us feedback!</a>
+									<a
+										href="/#feedback"
+										type="button"
+										className={`text-blueLike font-semibold `}
+										onClick={() => setCollapsed(true)}
+									>
+										Give us feedback!
+									</a>
 								</div>
 								<div className="w-1/3 ml-5 mb-5">
-									<Link className="text-l font-semibold text-blueLike" to="/" onClick={() => setCollapsed(true)}>
+									<Link
+										className="text-l font-semibold text-blueLike"
+										to="/"
+										onClick={() => setCollapsed(true)}
+									>
 										Home
 									</Link>
 								</div>
-								<a href="/#about" className="text-blueLike ml-5 mb-5"> About </a>
+								<a href="/#about" className="text-blueLike ml-5 mb-5">
+									{' '}
+									About{' '}
+								</a>
 								{sessionContext.userId ? (
 									<div className="w-1/3 ml-5 mb-5">
 										<Link className="text-l font-semibold text-blueLike" onClick={handleSignOut}>
@@ -144,21 +138,22 @@ function Navbar({ collapsed, setCollapsed }) {
 									</div>
 								) : (
 									<div className="w-1/3 ml-5 mb-5">
-										<Link className="text-l font-semibold text-blueLike" to="/auth " onClick={() => setCollapsed(true)}>
+										<Link
+											className="text-l font-semibold text-blueLike"
+											to="/auth "
+											onClick={() => setCollapsed(true)}
+										>
 											Sign In
 										</Link>
 									</div>
 								)}
 							</div>
-
 						</div>
 					</div>
 				</div>
-
 			) : (
 				<></>
 			)}
-
 		</div>
 	);
 }
