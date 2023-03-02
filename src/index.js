d6c230573423975cb7534854d9d789f14b3436b9
chinchin <86435165+chinekop@users.noreply.github.com>
2023-03-02 17:18:09 +0300

    minor

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  {/* <GoogleOAuthProvider clientId="1095799494177-qhg6sot0m532rg51j34kfrf3t0rds5sg.apps.googleusercontent.com"> */ }
-  < BrowserRouter >
-  <App />
-    </BrowserRouter >
-  {/* </GoogleOAuthProvider> */ }
+  <GoogleOAuthProvider clientId="1095799494177-qhg6sot0m532rg51j34kfrf3t0rds5sg.apps.googleusercontent.com">
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 );
