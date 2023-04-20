9bab4863c37cd7329fc879882cf094d21e585458
chinchin <86435165+chinekop@users.noreply.github.com>
2023-04-20 23:50:54 +0300

    toaster

--- src/components/Landing_page/Welcome.jsx
+++ src/components/Landing_page/Welcome.jsx
@@ -16,6 +16,23 @@ export default function Welcome() {
 	const [language, setLanguage] = useState('en-US');
 	const [loading, setLoading] = useState(false);
 	const { currentUser } = useAuth();
+	const [showToaster, setShowToaster] = useState(false);
+
+	useEffect(() => {
+		// check if the toaster has already been displayed
+
+
+		if (!showToaster) {
+			// if not, display the toaster and set a flag in localStorage
+			setShowToaster(true);
+			toast("⚠️ We are some encountering technical issues. You may encounter long waiting times or errors with your videos longer than 1 hour.", {
+				duration: 5000,
+				position: "top-center",
+				className: "text-xl	",
+				icon: ""
+			})
+		}
+	}, []);
 
 
 	/*
@@ -197,13 +214,17 @@ export default function Welcome() {
 			<Toaster />
 
 			<div className="items-center justify-center w-full lg:space-x-4 lg:flex">
+
 				<div className="flex items-center mt-4 space-x-4 md:justify-center lg:mt-0">
+
 					<div className="w-full flex flex-col">
+
 						<p class=" text-l mb-2 text-gray-600 dark:text-gray-400 pb-2 ">
 
 							<button id="popoverButton" data-popover-target="popoverDescription" data-popover-placement="left" data-popover-offset="20" type="button"><svg class="w-5 h-5 ml-1 pt-1 text-gray-400 hover:text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg><span class="sr-only">Show information</span></button>
 
 							Instructions for Alphy</p>
+
 						<div data-popover id="popoverDescription" role="tooltip" class={`popover-description absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-zinc-50 border border-slate-800 rounded-lg shadow-sm opacity-0 w-72`}>
 							<div class="p-3 space-y-2">
 								<h3 class="font-semibold text-gray-900">Before submitting</h3>
