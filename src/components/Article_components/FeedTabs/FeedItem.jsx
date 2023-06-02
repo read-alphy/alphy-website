4bcd6590ab491460170b64fe0667bab10b1cd7db
chinchin <86435165+chinekop@users.noreply.github.com>
2023-06-02 14:11:01 +0300

    minor fix

--- src/components/Article_components/FeedTabs/FeedItem.jsx
+++ src/components/Article_components/FeedTabs/FeedItem.jsx
@@ -15,7 +15,7 @@ const FeedItem = ({ item, setCollapsed, mainInputFeed }) => {
 		imageUrl = Twitter;
 	}
 
-
+console.log(item)
 
 	
 	const navigate = useNavigate();
@@ -52,7 +52,7 @@ const FeedItem = ({ item, setCollapsed, mainInputFeed }) => {
 					</div>
 
 					<div className="text-xs w-1/2">
-						{item.summaries!==undefined && item.summaries[0]!==undefined && item.summaries[0].summary!==null ? null : (
+						{item.summaries!==undefined && item.summaries[0]!==undefined && item.summaries[0].complete===true ? null : (
 							<div className="font-bold text-purpleLike dark:text-zinc-300">📝 IN PROGRESS</div>
 						)}	
 						<div className="text-sm video-text text-black dark:bg-mildDarkMode dark:text-zinc-300 font-normal">{item.title}</div>
