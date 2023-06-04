39e8b7bcf294c9beb0306bbc5a397607c9a29238
chinchin <86435165+chinekop@users.noreply.github.com>
2023-06-04 21:52:28 +0300

    minor fix

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -37,7 +37,7 @@ const Auth = () => {
 /* 		if (window.location.href=="http://localhost:3000/u/resetpassword" && resetPassword ===false){
 			navigate('/u/login')
 		} */
-		if (auth.currentUser) {
+		if (auth.currentUser && resetPassword===false) {
 			navigate('/');
 		}
 	
@@ -362,7 +362,7 @@ verificationMessage==false ?
 				<p className="text-sm text-zinc-600 mb-10">Enter your email address and we will send you a link to reset your password.</p>
 					<form onSubmit={sendResetPasswordMail}>
 					<div class="relative z-0 w-full mb-6 group">
-						<input value={email} onChange={handleEmailChange} type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+						<input value={auth.currentUser ? auth.currentUser.email : email} onChange={handleEmailChange} type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
 						<label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
 					</div>
 					<Button type="submit" className={`${isSubmitting ? "opacity-50 pointer-events-none" :""} bg-green-400`}> {isSubmitting  ? <ReactLoading type="spin" width={17} height={17} color="#ffffff"/> : <span>Send Link</span>}</Button>
