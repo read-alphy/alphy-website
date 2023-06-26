674d6ca02ceb14bc280d319ce5d40e1cf1a2f054
chinchin <86435165+chinekop@users.noreply.github.com>
2023-06-26 20:11:30 +0300

    added local files part to account & pricing pages

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -179,7 +179,7 @@ export default function Account({ credit,hasActiveSub}) {
                                             
                                         </div>
                                         <p className="mt-3 text-gray-400">Discover Alphy's capabilities </p>
-                                        <div className="h-[360px]">
+                                        <div className="h-[400px]">
                                             <ul role="list" className="space-y-5 my-7">
                                                 <li className="flex space-x-3">
 
@@ -271,7 +271,7 @@ export default function Account({ credit,hasActiveSub}) {
                                             <span className="ml-1 text-xl font-normal text-gray-500 dark:text-zinc-300">/month</span>
                                         </div>
                                         <p className="mt-3 text-gray-400">Level up your reach </p>
-                                        <div className="h-[360px]">
+                                        <div className="h-[400px]">
                                             <ul role="list" className="space-y-5 my-7">
                                                 <li className="flex space-x-3">
 
@@ -300,6 +300,11 @@ export default function Account({ credit,hasActiveSub}) {
                                                     {/* <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Optional credit top ups</span> */}
                                                     <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">No popularity limit</span>
                                                 </li>
+                                                <li className="flex space-x-3">
+                                                <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-400 dark:text-zinc-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                                                {/* <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Optional credit top ups</span> */}
+                                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Process local audio files</span>
+                                            </li>
                                                 <li className="flex space-x-3">
                                                     <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-400 dark:text-zinc-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
                                                     {/* <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Optional credit top ups</span> */}
@@ -508,12 +513,12 @@ export default function Account({ credit,hasActiveSub}) {
                                 <p className="mb-4 text-xl font-medium text-gray-500 dark:text-zinc-300">Premium</p>
                                     {/* <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-zinc-300">For Seekers</h5> */}
                                     <div className="flex items-baseline text-gray-900 dark:text-white">
-                                        <span className="text-l font-semibold">$</span>
-                                        <span className="text-2xl font-extrabold tracking-tight">5</span>
+                                        <span className="text-3xl font-semibold">$</span>
+                                        <span className="text-5xl font-extrabold ">5</span>
                                         <span className="ml-1 text-xl font-normal">/month</span>
                                     </div>
                                     <p className="mt-3 text-gray-400">Level up your reach </p>
-                                    <div className="h-[450px]">
+                                    <div className="h-[430px]">
                                         <ul role="list" className="space-y-5 my-7">
                                             <li className="flex space-x-3">
 
@@ -542,6 +547,11 @@ export default function Account({ credit,hasActiveSub}) {
                                                 <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-400 dark:text-zinc-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
                                                 {/* <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Optional credit top ups</span> */}
                                                 <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">No popularity limit</span>
+                                            </li>
+                                            <li className="flex space-x-3">
+                                                <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-400 dark:text-zinc-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                                                {/* <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Optional credit top ups</span> */}
+                                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Process local audio files</span>
                                             </li>
                                             <li className="flex space-x-3">
                                                     <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-400 dark:text-zinc-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
--- src/routes/Pricing.jsx
+++ src/routes/Pricing.jsx
@@ -148,7 +148,7 @@ export default function Pricing({ stripePromise,hasActiveSub }) {
                                             {/* <span className="ml-1 text-xl font-normal text-gray-500 dark:text-zinc-300">/month</span> */}
                                         </div>
                                         <p className="mt-3 text-gray-400">Discover Alphy's capabilities </p>
-                                        <div className="h-[360px]">
+                                        <div className="h-[400px]">
                                             <ul role="list" className="space-y-5 my-7">
                                                 <li className="flex space-x-3">
 
@@ -251,7 +251,7 @@ export default function Pricing({ stripePromise,hasActiveSub }) {
                                             <span className="ml-1 text-xl font-normal text-gray-500 dark:text-zinc-300">/month</span>
                                         </div>
                                         <p className="mt-3 text-gray-400">Level up your reach </p>
-                                        <div className="h-[360px]">
+                                        <div className="h-[400px]">
                                             <ul role="list" className="space-y-5 my-7">
                                                 <li className="flex space-x-3">
 
@@ -280,6 +280,11 @@ export default function Pricing({ stripePromise,hasActiveSub }) {
                                                     {/* <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Optional credit top ups</span> */}
                                                     <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">No popularity limit</span>
                                                 </li>
+                                                <li className="flex space-x-3">
+                                                <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-400 dark:text-zinc-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                                                {/* <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Optional credit top ups</span> */}
+                                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Process lcoal audio files</span>
+                                            </li>
                                                 <li className="flex space-x-3">
                                                     <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-400 dark:text-zinc-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
                                                     {/* <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Optional credit top ups</span> */}
@@ -481,6 +486,11 @@ export default function Pricing({ stripePromise,hasActiveSub }) {
                                                 <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-400 dark:text-zinc-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
                                                 {/* <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Optional credit top ups</span> */}
                                                 <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">No popularity limit</span>
+                                            </li>
+                                            <li className="flex space-x-3">
+                                                <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-400 dark:text-zinc-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                                                {/* <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Optional credit top ups</span> */}
+                                                <span className="text-base font-normal leading-tight text-gray-500 dark:text-zinc-300">Process local audio files</span>
                                             </li>
                                             <li className="flex space-x-3">
                                                     <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-green-400 dark:text-zinc-200" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
