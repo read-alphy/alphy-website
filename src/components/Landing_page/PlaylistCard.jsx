664e05270e2a845122991e62f7ec6ca11cf96711
chinchin <86435165+chinekop@users.noreply.github.com>
2023-07-07 14:09:10 +0300

    still thumbnail, minor edit

--- src/components/Landing_page/PlaylistCard.jsx
+++ src/components/Landing_page/PlaylistCard.jsx
@@ -34,17 +34,17 @@ export default function PlaylistCard({item,index,currentUser}){
                     <div className="mt-8">
                     <img width={160} src={item.thumbnail_url}/>
                     </div>}
-            <div className="mt-4">
+            <div className="mt-4 ">
                 <SmartToyIcon className="text-green-400 "/>
                 {currentUser!==undefined && item.user_id === currentUser.uid && 
                 
                 <EditIcon className="cursor-pointer ml-2 dark:text-zinc-400 text-zinc-500" onClick={handleIconClick}/>}
                 </div>
                 </div>
-            <p className="text-zinc-700 dark:text-zinc-200 text-l  overflow-hidden">
+            <p className="text-zinc-700 dark:text-zinc-200 text-lg mt-4 overflow-hidden ">
                 {item.name}
             </p>
-            <p className="text-zinc-500 dark:text-zinc-500 text-sm mt-2 overflow-hidden">
+            <p className="text-zinc-500 dark:text-zinc-500 text-sm mt-2 overflow-hidden font-light">
                 {item.description!=="null" && item.description}</p>
             </div>
         </div> 
