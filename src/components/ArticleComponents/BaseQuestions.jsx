bd4baa26863f12a2c8bea03b0e553f109238511b
chinchin <86435165+chinekop@users.noreply.github.com>
2023-11-09 15:44:20 +0300

    i hate fonts

--- src/components/ArticleComponents/BaseQuestions.jsx
+++ src/components/ArticleComponents/BaseQuestions.jsx
@@ -21,7 +21,7 @@ const added_ts = new Date(data.added_ts);
 								<div id="accordion-flush" data-active-classes="bg-white dark:bg-mildDarkMode text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
 									<h2 id="accordion-flush-heading-1">
 										<button onClick={(event) => handleBaseQAaccordion(event, index, item)} type="button" className="flex items-center justify-between w-full py-5 font-averta-semibold text-left text-zinc-700 border-b border-gray-200 dark:border-gray-700 dark:text-zinc-200 text-md sm:text-l	" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
-											<span className="font-averta-semibold">{item}</span>
+											<span className="font-averta-semibold text-md">{item}</span>
 											<svg data-accordion-icon className={`w-6 h-6 ${index === collapseIndex && collapseIndex !== -1 ? "rotate-180" : ""} shrink-0`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
 										</button>
 									</h2>
