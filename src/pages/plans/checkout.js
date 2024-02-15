4c4cb82b49306a81c73236807b1bbc8fad073b12
Egemen Göl <egemengol@gmail.com>
2024-02-15 16:08:01 +0300

    i swc,sharp & 404 page

--- src/pages/plans/checkout.js
+++ src/pages/plans/checkout.js
@@ -1,17 +1,25 @@
-import CheckoutPage from '../../components/Payment/CheckoutPage';
+import CheckoutPage from "../../components/Payment/CheckoutPage";
 
-
-export default function Checkout( {collapsed, setCollapsed, tier, currentUser, sandboxHistory, setSandboxHistory, loggedIn, setLoggedIn}) {
-    return (
-        <CheckoutPage
-        collapsed={collapsed}
-                    setCollapsed={setCollapsed}
-                    tier={tier}
-                    currentUser={currentUser}
-                    sandboxHistory={sandboxHistory}
-                    setSandboxHistory={setSandboxHistory}
-                    loggedIn ={loggedIn}
-                    setLoggedIn={setLoggedIn}
-                     />
-    );
-    }
+export default function Checkout({
+  collapsed,
+  setCollapsed,
+  tier,
+  currentUser,
+  sandboxHistory,
+  setSandboxHistory,
+  loggedIn,
+  setLoggedIn,
+}) {
+  return (
+    <CheckoutPage
+      collapsed={collapsed}
+      setCollapsed={setCollapsed}
+      tier={tier}
+      currentUser={currentUser}
+      sandboxHistory={sandboxHistory}
+      setSandboxHistory={setSandboxHistory}
+      loggedIn={loggedIn}
+      setLoggedIn={setLoggedIn}
+    />
+  );
+}
