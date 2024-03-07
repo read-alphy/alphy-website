4577c70fc4a976b84c0a48be66a62144ce5534e4
chinchin <86435165+chinekop@users.noreply.github.com>
2024-03-07 15:52:56 +0300

    size matter

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -81,6 +81,18 @@ function MyApp({ Component, pageProps }) {
       }, []);
 
       
+
+      useEffect(() => {
+        const appHeight = () => {
+          const doc = document.documentElement;
+          doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+      }
+      
+      window.addEventListener('resize', appHeight);
+      appHeight();
+      return () => window.removeEventListener('resize', appHeight);
+      }, []);
+      
       useEffect (() => {
         const theme = localStorage.getItem('theme')
         
