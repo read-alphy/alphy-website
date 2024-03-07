b1f8daf9fec51196bfee4e6d7a3c914e9cda6717
chinchin <86435165+chinekop@users.noreply.github.com>
2024-03-07 19:32:53 +0300

    ...

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" className="bg-white dark:bg-darkMode ">
+      <html lang="en" className="bg-white dark:bg-darkMode ">
         <Head>
           {/* Static files and scripts */}
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i&display=swap" />
@@ -18,7 +18,7 @@ class MyDocument extends Document {
           <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MSDBCTS');` }} />
           <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MSDBCTS" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
         </body>
-      </Html>
+      </html>
     )
   }
 }
