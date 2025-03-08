22c0921ad57a45c4bbf047a6036c99e25209e7dd
frkngk <hasanfurkan.gok@gmail.com>
2025-03-09 00:58:55 +0300

    metadata

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -5,14 +5,9 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Alphy",
-  description: "Convert audio to text, learn better with summaries and AI assistants, and use AI to create on top of YouTube, X Spaces, and Podcasts. Try Alphy for free!",
-};
-
 export default function RootLayout({ children }) {
 
-    console.log("heyman")
+    
   return (
     <>
       
